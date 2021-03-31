571d3539b1850f47b76a34273b7c7a2a0530d379
twang08 <twang08@syr.edu>
2021-03-30 21:31:14 -0700

    added deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t cygnetops/multi-client-k8s:latest -t cygnetops/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
-docker build -t cygnetops/multi-server-k8s-pgfix:latest -t cygnetops/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
-docker build -t cygnetops/multi-worker-k8s:latest -t cygnetops/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+docker build -t twang76912/multi-client-k8s:latest -t twang76912/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t twang76912/multi-server-k8s-pgfix:latest -t twang76912/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
+docker build -t twang76912/multi-worker-k8s:latest -t twang76912/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+# as we already login docker in travis in before_install, so no need to login again
+docker push twang76912/multi-client-k8s:latest
+docker push twang76912/multi-server-k8s-pgfix:latest
+docker push twang76912/multi-worker-k8s:latest
 
-docker push cygnetops/multi-client-k8s:latest
-docker push cygnetops/multi-server-k8s-pgfix:latest
-docker push cygnetops/multi-worker-k8s:latest
-
-docker push cygnetops/multi-client-k8s:$SHA
-docker push cygnetops/multi-server-k8s-pgfix:$SHA
-docker push cygnetops/multi-worker-k8s:$SHA
+docker push twang76912/multi-client-k8s:$SHA
+docker push twang76912/multi-server-k8s-pgfix:$SHA
+docker push twang76912/multi-worker-k8s:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=cygnetops/multi-server-k8s-pgfix:$SHA
-kubectl set image deployments/client-deployment client=cygnetops/multi-client-k8s:$SHA
-kubectl set image deployments/worker-deployment worker=cygnetops/multi-worker-k8s:$SHA
+kubectl set image deployments/server-deployment server=twang76912/multi-server-k8s-pgfix:$SHA
+kubectl set image deployments/client-deployment client=twang76912/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=twang76912/multi-worker-k8s:$SHA
